9a7a5db852bf271efacfba1eaa03f35f4d9c01de
NGABO <ngaboizo2000@gmail.com>
2025-06-01 11:03:39 +0200

    generated html with javascript

---
+++ data/amazon.js
@@ -0,0 +1,49 @@
+let productHTML = '';
+
+
+//2.generate the HTML
+products.forEach((product) => {
+  
+productHTML+=
+`
+  <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image" src="${product.image}"></div>
+        <div class="product-name-container">${product.name}</div>
+      <div class="product-rating-container">
+        <img src="images/ratings/rating-${product.rating.stars * 10}.png " class="product-rating-image">
+        <div class="product-count">${product.rating.count}</div>
+      </div>
+      <div class="product-price-container">$${product.priceCents / 100}</div>
+      <div class="product-quantity-container">
+        <select class="select-button">
+          <option value="1" selected>1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+      <div class="added-to-cart-message"></div>
+      <button class="add-to-cart-button">Add to Cart</button>
+      </div>
+  `;
+ console.log(productHTML);
+})
+  
+
+
+
+//4.use dom to put html to page
+document.querySelector('.js-product-grid').innerHTML = productHTML;
+
+document.querySelectorAll('.add-to-cart-button').forEach((button) => {
+  button.addEventListener('click',() =>{
+    console.log('added product');
+  })
+});
