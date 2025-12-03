041710b38b2bdc3958ac1f126a9f3cdfb0439c40
ngabo <ngaboizo2000@gmail.com>
2025-12-03 19:03:43 +0200

    create class version of cart

---
+++ data/cart-class.js
@@ -0,0 +1,104 @@
+export class Cart{
+  cartItems ;
+  localStorageKey;
+
+ 
+  constructor(localStorageKey){
+    this.localStorageKey;
+    this.loadFromStorage()
+  }
+
+  loadFromStorage(){
+    JSON.parse(localStorage.getItem(this.localStorageKey));
+
+    if(!this.cartItems){
+      this.cartItems = [
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: 2
+      }
+    ];
+    }
+  }
+
+  saveToStorage(){
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+addToCart(productId){
+   let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if( cartItem.productId === productId ){
+      matchingItem = cartItem;
+     // console.log('matchingItem:',matchingItem)
+      }
+    })
+
+    if(matchingItem){
+      matchingItem.quantity += 1;
+     //console.log('matchingItem: ',matchingItem')
+    }else{
+      this.cartItems.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionId: 1
+    });
+    }
+   
+    this.saveToStorage();
+}
+
+removeFromCart(productId){
+  let newCart = [];
+  this.cartItems.forEach((cartItem) => {
+    if(cartItem.productId === productId){
+      return //skip the deleted cartItem
+    }else{
+      newCart.push(cartItem);
+    }
+  })
+  this.cartItems = newCart;
+
+  this.saveToStorage()
+}
+
+ updateDeliveryOption(deliveryOptionId, productId){
+  let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if( cartItem.productId === productId ){
+      matchingItem = cartItem;
+     // console.log('matchingItem:',matchingItem)
+      }
+    })
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    this.saveToStorage();
+}
+
+}
+
+
+
+
+
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business')
+
+
+// moved this code inside constructor
+// cart.localStorageKey = 'cart-oop';
+// businessCart.loaclStorageKey = 'cart-business'
+
+console.log(cart)
+console.log(businessCart)
+cart.loadFromStorage();
+
+
+
+
+
+
+
